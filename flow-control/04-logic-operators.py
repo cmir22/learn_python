95afe6c7c4a056dfef86e7d625bdf6e9258d9c1c
Cruz Ibarra <jackzelda_vicio@hotmail.com>
2023-03-04 15:24:33 -0800

    started to work with for loop

--- flow-control/04-logic-operators.py
+++ flow-control/04-logic-operators.py
@@ -1,1 +1,18 @@
 # and, or , not
+
+gas = True
+on = True
+age = 18
+
+
+if gas and on:
+    print('Able to turn on')
+
+if gas or on:
+    print('Able to turn on')
+
+if not gas and on and age > 17:
+    print('Able to turn on and age required')
+
+if gas and (on or age > 17):
+    print('Able to turn on and age required')
---
+++ flow-control/05-comparator-string.py
@@ -0,0 +1,4 @@
+age = 25
+
+if 15 <= age <= 65:
+    print('Can use pool')
