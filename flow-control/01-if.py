e726c89443740037622635cd661e1ba690f59fa0
Cruz Ibarra <jackzelda_vicio@hotmail.com>
2023-03-04 15:06:59 -0800

    Working with operators

---
+++ flow-control/01-if.py
@@ -0,0 +1,10 @@
+age = 15
+
+if (age > 17):
+    print('Can see this movie')
+elif (age > 50):
+    print('Discount to see this movie')
+else:
+    print('No required age')
+
+print('Done')
---
+++ flow-control/03-ternary.py
@@ -0,0 +1,10 @@
+age = 18
+
+message = "Is adult" if age > 17 else "Is too young"
+
+# if (age > 17):
+#     message = ("Is adult")
+# else:
+#     message = ('Too young')
+
+print(message)
