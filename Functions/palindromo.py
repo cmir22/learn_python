adeac0d261c1c3ec02baa4aaf5f44d6d23db231a
Cruz Ibarra <cruzibarra@Cruzs-Mac-mini.local>
2023-03-06 20:20:01 -0800

    Polindromo test

--- Functions/palindromo.py
+++ Functions/palindromo.py
@@ -1,6 +1,17 @@
-def is_palindromo(text):
-    for _ in text:
-        print(_)
+def invert_text(text):
+    text_without_spaces = remove_spaces(text)
+    inverted_text = ""
+    for txt in text_without_spaces:
+        inverted_text = txt + inverted_text
+    print(inverted_text.lower() == text_without_spaces.lower())
 
 
-is_palindromo("Hello world")
+def remove_spaces(text):
+    new_text = ""
+    for txt in text:
+        if (txt != " "):
+            new_text += txt
+    return new_text
+
+
+invert_text("moom")
