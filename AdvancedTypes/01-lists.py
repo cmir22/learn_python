9bd3f687744ad0599d793a482b08bf3bddf8154c
Cruz Ibarra <cruzibarra@Cruzs-Mac-mini.local>
2023-03-06 20:44:11 -0800

    Wroking with lists

---
+++ AdvancedTypes/01-lists.py
@@ -0,0 +1,14 @@
+numbers = [1, 2, 3]
+letters = ["a", "b", "c"]
+words = ["words", "hello", "chanchito"]
+booleans = [True, False, False, False, True]
+matrix = [[0, 1], [1, 0]]
+zero = [0] * 10
+# Join lists
+alphanumeric = numbers + letters
+# Create list of 10 numbers
+ranger = list(range(1, 11))
+#
+chars = list("hello world")
+
+print(chars)
---
+++ AdvancedTypes/02-manage-lists.py
@@ -0,0 +1,9 @@
+pets = ["Chuchis", "Donita", "Dogo"]
+
+pets[0] = "Bicho"
+
+
+print(pets)
+print(pets[0:2])
+print(pets[::2])
+print(pets[1:2:2])
